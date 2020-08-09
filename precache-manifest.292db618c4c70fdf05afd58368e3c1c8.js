b5bb1bac32a6ddbafc38f118f71c07f95cc621ec
root <root@DESKTOP-LC1EVA9.localdomain>
2020-08-09 11:27:12 +0800

    Updates

--- precache-manifest.292db618c4c70fdf05afd58368e3c1c8.js
+++ precache-manifest.292db618c4c70fdf05afd58368e3c1c8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6015a0083d7c3e349faf60d3189ca1b0",
+    "revision": "ec9796c990f1ecaf9d0d0c38ad435408",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "8eed12a71b1a844816e4",
+    "revision": "c93215826bdf3c9287d0",
     "url": "/portfolio/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.4c736102.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8eed12a71b1a844816e4",
-    "url": "/portfolio/static/js/main.10a1b4a7.chunk.js"
+    "revision": "c93215826bdf3c9287d0",
+    "url": "/portfolio/static/js/main.7f4513b5.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
