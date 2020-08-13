1191c7cc409bd94f6135018524190e5472d5deb5
root <root@DESKTOP-LC1EVA9.localdomain>
2020-08-13 21:24:26 +0800

    Updates

---
+++ precache-manifest.3bdface828b117cc976ac2dd25c27cd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a7ad14c0a1e6e2d047f10839a89523",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5ba41d0eb6d0c2407c76",
+    "url": "/portfolio/static/css/main.34fbadb2.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ba41d0eb6d0c2407c76",
+    "url": "/portfolio/static/js/main.f4eebceb.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
